a23db8e909de806fb0365665e5f92bf15ad9dd3f
UYulong <1274583083@qq.com>
2022-10-16 11:25:31 +0800

    deps(icon): add ionicons5 deps

--- components/NavFooter.vue
+++ components/NavFooter.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     footer2
+
   </div>
 </template>
 
 <script lang="ts" setup name=''>
-
+import { NIcon } from 'naive-ui'
+import { Search } from '@vicons/ionicons5'
 </script>
 
 <style lang="scss" scoped>
